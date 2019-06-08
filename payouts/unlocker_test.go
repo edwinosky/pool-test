b240ea169db021a3ffbc8cbcd373f128865e2c78
Edwin Corleone <edwinpitters@yahoo.com>
2019-06-08 13:36:54 -0400

    Update unlocker_test.go

--- payouts/unlocker_test.go
+++ payouts/unlocker_test.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/chainkorea/open-callisto-pool/rpc"
-	"github.com/chainkorea/open-callisto-pool/storage"
+	"github.com/edwinosky/open-pool/rpc"
+	"github.com/edwinosky/open-pool/storage"
 )
 
 func TestMain(m *testing.M) {
